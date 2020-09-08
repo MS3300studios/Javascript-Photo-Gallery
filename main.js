40b1c29bb287ac0cd9fffb98982da634fec28c86
Mikołaj Strusiński <mikistrus@gmail.com>
2020-09-08 12:36:08 +0200

    Changed Styling and added comments
    descriptive comments are now added to the javascript code.
    minor style improvements added

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ var left = document.getElementById('btnLeft')
 var right = document.getElementById('btnRight')
 var x = document.getElementById('btnX')
 
+//an array of sources of the images 
 var imgSourceList = []
 for(let i=0; i<imgList.length; i++){
     imgSourceList.push(imgList[i].src)
 }
 
-// list of li elements that contain descriptions
+//an array of li elements that contain descriptions
 var descList = document.querySelectorAll('#descList li')
 // list of innerHTML extracted from li elements above
 var descTxtList = []
@@ -21,11 +22,11 @@ for(let i=0; i<descList.length; i++){
 }
 
 //image changing with buttons
-var currentCell = null
+var currentCell = null //the index of the photo currently on the screen
 var lastCell = imgList.length-1
 var firstCell = 0
-var direction = null
-var currentSource = null
+var direction = null //determines wether the user wants the next image (true) of previous image (false)
+var currentSource = null //the current src of the image, it is used to get the currentCell (index) of the image
 var newCell = null
 
 
@@ -35,10 +36,10 @@ for(let i=0; i<imgList.length; i++){
     imgList[i].addEventListener('click',function(e){
         lightbox.style.display = 'flex'
         lightboxImage.src = e.target.src
-
         currentSource = lightboxImage.src
-        let descSource = imgSourceList.indexOf(currentSource)
-        description.innerHTML = descTxtList[descSource]
+        //get the index of the image and use it to get the correct description for it
+        let descriptionCell = imgSourceList.indexOf(currentSource)
+        description.innerHTML = descTxtList[descriptionCell]
     })
 }
 
@@ -50,15 +51,15 @@ x.addEventListener('click', function(){
 function changeImage(currSrc,dir){
     currentCell = imgSourceList.indexOf(currSrc)
 
-    if(currentCell==lastCell){
+    if(currentCell==lastCell){ //this is the last image, the 'next image' is disable
         if(dir == false){
             newCell = currentCell-1
-            lightboxImage.src = imgSourceList[newCell]
-            description.innerHTML = descTxtList[newCell]
-            currentSource = imgSourceList[newCell]
+            lightboxImage.src = imgSourceList[newCell]   //set the correct image
+            description.innerHTML = descTxtList[newCell] //set the correct description
+            currentSource = imgSourceList[newCell]       //update the source (on which the index is determined)
         }
     }
-    else if(currentCell==firstCell){
+    else if(currentCell==firstCell){ //this is the first image, the 'previous image' is disabled
         if(dir == true){
             newCell = currentCell+1
             lightboxImage.src = imgSourceList[newCell]
@@ -66,7 +67,7 @@ function changeImage(currSrc,dir){
             currentSource = imgSourceList[newCell]
         }
     }
-    else{
+    else{ //these are the middle images, previous and next image are enabled
         if(dir == true) newCell = currentCell+1
         else newCell = currentCell-1
 
